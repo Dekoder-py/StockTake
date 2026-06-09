4e03a0ce8029e1050b2aab132f80d831ad1a503c
Dekoder-py <kyle@codingcorner.dev>
2026-06-09 14:42:28 +1200

    add entry field

--- src/main/java/dev/codingcorner/Panel.java
+++ src/main/java/dev/codingcorner/Panel.java
@@ -2,8 +2,12 @@ package dev.codingcorner;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Panel extends JPanel {
 
@@ -11,9 +15,30 @@ public class Panel extends JPanel {
   public static final int HEIGHT = 600;
 
   public Panel() {
-
     this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
     this.setBackground(Color.WHITE);
+    this.setLayout(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.insets = new Insets(5, 5, 5, 5);
+
+    JLabel title = new JLabel("StockTake");
+    gbc.gridy = 0;
+    this.add(title, gbc);
+
+    JTextField addItemField = new JTextField(6);
+    gbc.gridy = 1;
+
+    addItemField.addActionListener(e -> {
+      onAddItem();
+      addItemField.setText("");
+	});
+
+    this.add(addItemField, gbc);
+
+  }
+
+  private void onAddItem() {
+    System.out.println("item added ig");
   }
 
 }

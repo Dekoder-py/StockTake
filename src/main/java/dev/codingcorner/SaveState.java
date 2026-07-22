87d0e8c7fa8bdd8a3b1a8ddf719e7cd6ab6743cf
Dekoder-py <kyle@codingcorner.dev>
2026-07-22 12:43:35 +1200

    create save state class

---
+++ src/main/java/dev/codingcorner/SaveState.java
@@ -0,0 +1,12 @@
+package dev.codingcorner;
+
+import java.util.Vector;
+
+public class SaveState {
+
+  public void saveToFile(String filePath, Vector<InventoryItem> items) {
+  }
+
+  public Vector<InventoryItem> loadFromFile(String filePath) {
+  }
+}
